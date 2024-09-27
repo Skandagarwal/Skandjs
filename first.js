0687beadb0b7f3469b12916d60d3178cf15ce7d8
Skandagarwal <skandagarwal0503@gmail.com>
2024-09-27 23:12:41 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,14 @@
+const Skand=1321
+let SkandID=6278777
+Skandcity = "rampur"
+let Skandwife;
+var skandmom= "Anju Agarwal"
+SkandID=62761788183
+
+/* prefer not to use var
+*/
+
+//  console.log(Skand)
+// console.log(SkandID);
+console.table([Skand,SkandID,Skandcity,Skandwife,skandmom]);
+
